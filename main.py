5b5974446c1dcbdc979a0c1357351e9c6be2105e
satwinder-dev <satwinder-dev@users.noreply.github.com>
2026-03-05 18:53:46 +0530

    Add /dev CRUD endpoints and sample data

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
 from fastapi import FastAPI
-
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+dc = [
+    {
+        "id" : 1,
+        "name": "Satwinder",
+        "tech": ["React", "Next.js"],
+        "city": "Patiala"
+    },
+    {
+        "id" : 2,
+        "name": "Rahul",
+        "tech": ["Python", "FastAPI"],
+        "city": "Delhi"
+    },
+    {   
+        "id" : 3,
+        "name": "Ankit",
+        "tech": ["Node.js", "Express"],
+        "city": "Mumbai"
+    },
+    {
+        "id" : 4,
+        "name": "Priya",
+        "tech": ["Angular", "TypeScript"],
+        "city": "Bangalore"
+    },
+    {
+        "id" : 5,
+        "name": "Simran",
+        "tech": ["Django", "PostgreSQL"],
+        "city": "Chandigarh"
+    }
+]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/dev")
+def read_root():
+    print(dc)
+    return dc
+
+@app.get("/dev/{dev_id}")
+def get_dev(dev_id:int):
+    for dev in dc:
+        if dev_id == dev["id"]:
+            print(dev)
+            return dev
+
+@app.post("/dev")
+def create_dev(dev : dict):
+    dc.append(dev)
+    return {"message":"dev added", "data":dev}
+
+@app.put("/dev/{dev_id}")
+def update(dev_id:int , updated_dev : dict):
+    for index,dev in enumerate(dc):
+        if dev["id"] == dev_id:
+            dc[index] = updated_dev
+            print(update)
+            return {"message" : "dev updated", "data" : updated_dev}
+        
+@app.delete("/dev/{dev_id}")
+def delete_dev( dev_id : int):
+    for index,dev in enumerate(dc):
+        if dev["id"] == dev_id:
+           deleted= dc[index]
+           dc.pop(index)
+           print(deleted)
+           return deleted
